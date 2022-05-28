825892f7a2941ad0a369ad3b0325fa8007bbf663
Andreskub <andreskubler@gmail.com>
2022-05-28 18:48:55 -0300

    Some features

--- js/main.js
+++ js/main.js
@@ -6,63 +6,54 @@ class Investment {
   }
 }
 
-class Client {
-  constructor(name, document) {
-    this.name = name;
-    this.document = document;
-    this.investments = [];
-  }
+/* ---------------------------------------------------------------------------- */
+
+function Investment2(id, name, img, price) {
+  this.id = id;
+  this.name = name;
+  this.img = img;
+  this.price = price;
+  this.quantity = 0;
+  this.subtotal = 0;
+  this.subTotal = () => this.subtotal = this.price * this.quantity;
+  this.decreaseQuantity = () => this.quantity--;
+  this.increaseQuantity = () => this.quantity++;
 }
 
-const client = new Client("Usero01", "123456789")
+const $products = document.querySelector(".products");
+const $cart = document.querySelector(".cart-items");
+const $subtotal = document.querySelector(".subtotal");
+const $totalItems = document.querySelector(".total-items-in-cart");
 
-function getInputValues() {
-  let moneyInvested = parseInt(document.getElementById("amount_to_invest").value);
-  let daysInvested = parseInt(document.getElementById("days_to_invest").value);
+/*
+const listInvestments = [];
 
-  let investMentReturn = Math.round((moneyInvested * 43 * daysInvested)/36500);
-
-  return [moneyInvested, daysInvested, investMentReturn]
+data.forEach(e => {
+  listInvestments.push(new Investment2(e.id, e.name, e.img, e.price))
+});
+*/
+function renderInvestments() {
+  data.forEach((product) => {
+    $products.innerHTML += `
+            <div class="col-lg-6 col-sm-12 item">
+              <div class="item-container">
+                <div class="item-img">
+                  <img src="${product.img}" alt="${product.name}">
+                </div>
+                <div class="desc">
+                  <h2>${product.name}</h2>
+                  <h2><small>$</small>${product.price}</h2>
+                </div>
+                <div class="add-to-cart" onclick="addToCart(${product.id})">
+                  <img src="./assets/cart.png" alt="add to cart">
+                </div>
+              </div>
+            </div>
+        `;
+  });
 }
 
-const clearInputValues = () => {
-  document.getElementById("amount_to_invest").value = "";
-  document.getElementById("days_to_invest").value = "";
-}
-
-const calculateInvestment = button => {
-  button.preventDefault();
-
-  let values = getInputValues();
-  
-  let responseText = document.getElementById("queryResult");
-  console.log(responseText);
-  responseText.innerText = "You will recive a total of " + (values[0] + values[2]) + " dollars in " + values[1] + " days with an investment return of "+ values[2] + " dollars!";
-
-  clearInputValues();
-}
-
-let calculateButton = document.getElementById("calculateInvestmentButton");
-calculateButton.addEventListener("click", calculateInvestment);
-
-
-const createNewInvestment = button => {
-  button.preventDefault();
-
-  let values = getInputValues();
-
-  const newInvestment = new Investment(values[0], values[1], values[2]);
-  client.investments.push(newInvestment);
-
-  let responseText = document.getElementById("queryResult");
-  console.log(responseText);
-  responseText.innerText = "Your new investment was registered correctly!";
-
-  console.log("Client");
-  console.log(client);
-
-  clearInputValues();
-}
-
-let createInvestmentButton = document.getElementById("newInvestmentButton");
-createInvestmentButton.addEventListener("click", createNewInvestment);
+document.addEventListener("DOMContentLoaded", () => {
+  renderInvestments();
+  setCart();
+});
---
+++ js/products.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    id: 1,
+    name: "Plazo Fijo",
+    investmentReturn: 300,
+    img: "./assets/bitcoin-transparent.png",
+    price: 300
+  },
+  {
+    id: 2,
+    name: "Plazo Fijo a Corto Plazo",
+    investmentReturn: 200,
+    img: "./assets/bitcoin-transparent.png",
+    price: 300
+  },
+  {
+    id: 3,
+    name: "Plazo Fijo a Largo Plazo",
+    investmentReturn: 200,
+    img: "./assets/bitcoin-transparent.png",
+    price: 300
+  }
+];
